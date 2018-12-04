675d6981c0c5d014b363380daabc7595ccf33888
David <david.windler@windowslive.com>
2018-12-04 21:55:17 +0100

    Create pages from markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,62 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// You want to use each markdown file name to create the page slug. So pandas.md will become /pandas/.
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({ node, getNode, basePath: 'pages' })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  // query all markdown pages
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              templateKey
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(
+          `src/templates/${String(node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        context: {
+          id: node.id,
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
--- src/templates/index.js
+++ src/templates/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/gartenbau">Go to page 2</Link>
   </Layout>
 )
 
