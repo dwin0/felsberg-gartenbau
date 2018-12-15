532a2678913af313ed8307a0fc8b2f51c2a6600b
David <david.windler@windowslive.com>
2018-12-15 02:01:38 +0100

    Added navigation entries

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,66 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { StaticQuery, graphql, Link } from 'gatsby'
+
+import styled from 'styled-components'
+
+const Navigation = styled.nav`
+  display: flex;
+`
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <h1>
-      <Link to="/">{siteTitle}</Link>
-      <nav>{/* TODO: */}</nav>
-    </h1>
-  </header>
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          filter: {
+            frontmatter: { linkInNavigation: { visible: { eq: true } } }
+          }
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                linkInNavigation {
+                  order
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={({ allMarkdownRemark: { edges } }) => {
+      const navigationEntries = edges
+        .sort(
+          (edgeA, edgeB) =>
+            edgeA.node.frontmatter.linkInNavigation.order -
+            edgeB.node.frontmatter.linkInNavigation.order,
+        )
+        .map(edge => ({
+          link: edge.node.fields.slug,
+          title: edge.node.frontmatter.title,
+        }))
+
+      return (
+        <header>
+          <h1>
+            <Link to="/">{siteTitle}</Link>
+            <Navigation>
+              {navigationEntries.map(({ link, title }) => (
+                <Link key={title} to={link}>
+                  {title} |
+                </Link>
+              ))}
+            </Navigation>
+          </h1>
+        </header>
+      )
+    }}
+  />
 )
 
 Header.propTypes = {
