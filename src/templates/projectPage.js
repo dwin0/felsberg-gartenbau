7f1d79206a4ac285e802b7ffa187720a10762a5e
David <david.windler@windowslive.com>
2018-12-16 14:46:17 +0100

    Improved projektseite

--- src/templates/projectPage.js
+++ src/templates/projectPage.js
@@ -1,5 +1,39 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 
-const ProjectPage = () => <p>Projekt</p>
+import Layout from '../components/Layout'
+
+const ProjectPage = ({ data }) => {
+  const { title } = data.markdownRemark.frontmatter
+
+  return (
+    <Layout>
+      <Layout.ContentWrapper>
+        <p>{title}</p>
+      </Layout.ContentWrapper>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+
+ProjectPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+}
 
 export default ProjectPage
