f47599c8b97b92f40d63fed002785482560124c6
David <david.windler@windowslive.com>
2019-01-11 14:54:20 +0100

    Improved AllTags page structure

--- src/pages/projekte/tags.js
+++ src/pages/projekte/tags.js
@@ -1,52 +1,45 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { Link, graphql, StaticQuery } from 'gatsby'
 
 import Layout from '../../components/Layout'
 
-const TagsPage = ({
-  data: {
-    allMarkdownRemark: { group },
-  },
-}) => (
-  <Layout>
-    <Layout.ContentWrapper>
-      <h1>Projekte nach Stichwörtern</h1>
-      <ul>
-        {group.map(tag => (
-          <li key={tag.fieldValue}>
-            <Link to={`projekte/tags/${tag.fieldValue.toLowerCase()}/`}>
-              {tag.fieldValue} ({tag.totalCount})
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Layout.ContentWrapper>
-  </Layout>
+const TagsPage = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          filter: { frontmatter: { templateKey: { eq: "projectPage" } } }
+        ) {
+          group(field: frontmatter___tags) {
+            fieldValue
+            totalCount
+          }
+          totalCount
+        }
+      }
+    `}
+    render={({ allMarkdownRemark: { group, totalCount: sumOfProjects } }) => (
+      <Layout>
+        <Layout.ContentWrapper>
+          <h1>Projekte nach Stichwort</h1>
+
+
+          <ul>
+            {group.map(({ fieldValue, totalCount }) => (
+              <li key={fieldValue}>
+                <Link to={`projekte/tags/${fieldValue.toLowerCase()}/`}>
+                  {fieldValue} ({totalCount})
+                </Link>
+              </li>
+            ))}
+            <li>
+              <Link to="projekte">Alle Projekte ({sumOfProjects})</Link>
+            </li>
+          </ul>
+        </Layout.ContentWrapper>
+      </Layout>
+    )}
+  />
 )
 
-TagsPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      group: PropTypes.arrayOf(
-        PropTypes.shape({
-          fieldValue: PropTypes.string.isRequired,
-          totalCount: PropTypes.number.isRequired,
-        }).isRequired,
-      ),
-    }),
-  }),
-}
-
 export default TagsPage
-
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark {
-      group(field: frontmatter___tags) {
-        fieldValue
-        totalCount
-      }
-    }
-  }
-`
