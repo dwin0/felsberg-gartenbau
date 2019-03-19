f04092e8d005adfeb736e8df46ede7016bac920a
David <david.windler@windowslive.com>
2019-03-19 12:35:13 +0100

    UL styling

--- src/components/common/CMS_Html.js
+++ src/components/common/CMS_Html.js
@@ -33,6 +33,22 @@ const CMS_HTML = styled.div`
   img {
     max-width: 100%;
   }
+
+  ul {
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    margin: -20px 0 40px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: fit-content;
+  }
+
+  li {
+    text-align: left;
+    line-height: 1.5;
+  }
 `
 
 export default CMS_HTML
